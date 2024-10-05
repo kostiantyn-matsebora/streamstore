fff3aecca3e7da94caded2e2c81e4554bde123a5
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-05 13:16:21 +0300

    Make metadata dictionary readonly

--- src/StreamStore.S3/AmazonS3/AmazonStreamLockMetadata.cs
+++ src/StreamStore.S3/AmazonS3/AmazonStreamLockMetadata.cs
@@ -9,7 +9,7 @@ namespace StreamStore.S3.AmazonS3
     internal class AmazonStreamLockMetadata
     {
         const string LockAcquiredAt = "x-amz-meta-stream-lock-acquired-at";
-        Dictionary<string, string> metadata = new Dictionary<string, string>();
+        readonly Dictionary<string, string> metadata = new Dictionary<string, string>();
 
         public string this[string key] => metadata[key];
 
