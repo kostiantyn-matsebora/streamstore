304f3146e6f7cb2b724b046185293ce9f6c7e09d
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-07 00:21:01 +0300

    Remove amazon s3 implementation and other redundant classes

--- src/StreamStore.S3/Client/IS3MetadataCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StreamStore.S3.Client
-{
-    public interface IS3MetadataCollection : IS3ReadonlyMetadataCollection
-    {
-        public IS3MetadataCollection Add(string key, string value);
-    }
-}
