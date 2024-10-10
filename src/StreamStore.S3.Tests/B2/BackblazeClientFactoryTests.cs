d04e87c1e7a33e9414a9eb7d8a292a9cb12e8175
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-10 21:23:23 +0300

    Add unit tests

---
+++ src/StreamStore.S3.Tests/B2/BackblazeClientFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Bytewizer.Backblaze.Client;
+using FluentAssertions;
+using Moq;
+using StreamStore.S3.B2;
+
+namespace StreamStore.S3.Tests.B2
+{
+    public class BackblazeClientFactoryTests
+    {
+        BackblazeClientFactory CreateFactory()
+        {
+            return new BackblazeClientFactory();
+        }
+
+        [Fact]
+        public void Create_Should_CreateBackblazeClient()
+        {
+            // Arrange
+            var factory = CreateFactory();
+
+            // Act
+            var result = factory.Create();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<BackblazeClient>();
+
+        }
+    }
+}
---
+++ src/StreamStore.S3.Tests/Integration/AWS/AWSS3StreamDatabaseIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿using StreamStore.Testing;
+
+namespace StreamStore.S3.Tests.Integration.AWS
+{
+    public class AWSS3StreamDatabaseIntegrationTests : StreamDatabaseTestsBase
+    {
+        public AWSS3StreamDatabaseIntegrationTests() : base(new AWSS3TestsSuite())
+        {
+        }
+    }
+}
