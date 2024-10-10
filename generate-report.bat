01eac839455ef90a6c775151d08c387291076efa
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-10 21:37:24 +0300

    Remove cmd files

--- generate-report.bat
+++
@@ -1,2 +0,0 @@
-dotnet-coverage collect 'dotnet test src --verbosity detailed --logger:trx --results-directory output/test-results' -f xml -o 'output/coverage.xml'
-reportgenerator -reports:"output/coverage.xml" -targetdir:"output/coverage-report" -reporttypes:Html
--- generate-report.cmd
+++
@@ -1,2 +0,0 @@
-dotnet-coverage collect "dotnet test src --verbosity normal --logger:trx --results-directory output/test-results" -f xml -o "output/coverage.xml"
-reportgenerator -reports:"output/coverage.xml" -targetdir:"output/coverage-report" -reporttypes:Html -assemblyfilters:"-*Tests*;"
