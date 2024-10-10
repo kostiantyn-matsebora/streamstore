13ff4afd767cef81399c370994073ba6ad6db704
kostiantyn-matsebora <7667108+kostiantyn-matsebora@users.noreply.github.com>
2024-10-10 21:25:35 +0300

    Codecoverage (#22)

---
+++ src/StreamStore.S3.AWS/AWSS3Factory2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Amazon.S3;
+using StreamStore.S3.Client;
+using StreamStore.S3.Lock;
+
+namespace StreamStore.S3.AWS
+{
+    internal class AWSS3Factory2 : S3FactoryBase
+    {
+        readonly AWSS3DatabaseSettings settings;
+
+        public AWSS3Factory2(AWSS3DatabaseSettings settings)
+        {
+            this.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+
+        public override IS3Client CreateClient()
+        {
+            return new AWSS3Client(new AmazonS3Client(), settings);
+        }
+    }
+}
