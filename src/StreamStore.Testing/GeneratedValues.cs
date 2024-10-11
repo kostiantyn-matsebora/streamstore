52d68ad5a523db3939d1399f19a4a2132c269609
kostiantyn-matsebora <7667108+kostiantyn-matsebora@users.noreply.github.com>
2024-10-11 14:39:49 +0300

    Add more test coverage (#25)

--- src/StreamStore.Testing/GeneratedValues.cs
+++ src/StreamStore.Testing/GeneratedValues.cs
@@ -1,12 +1,21 @@
 ﻿using AutoFixture;
 using StreamStore.S3;
-using StreamStore.Serialization;
+
 
 namespace StreamStore.Testing
 {
-    public static  class GeneratedValues
+    public static class GeneratedValues
     {
         public static string String => new Fixture().Create<string>();
+
+        public static DateTime DateTime => DateTime.UtcNow;
+        public static int Int
+        {
+            get
+            {
+                return new Random().Next(Int32.MinValue, Int32.MaxValue);
+            }
+        }
 
         public static byte[] ByteArray => Converter.ToByteArray(String);
 
