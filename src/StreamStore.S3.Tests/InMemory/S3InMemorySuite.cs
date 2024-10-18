61162c454c8ed2abf4ed423ad844eb13ecb4787b
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-18 16:42:20 +0300

    Uncomment S3 tests

--- src/StreamStore.S3.Tests/InMemory/S3InMemorySuite.cs
+++ src/StreamStore.S3.Tests/InMemory/S3InMemorySuite.cs
@@ -1,12 +1,12 @@
-﻿//using StreamStore.Testing;
+﻿using StreamStore.Testing;
 
-//namespace StreamStore.S3.Tests.InMemory
-//{
-//    internal class S3InMemorySuite : ITestSuite
-//    {
-//        public IStreamDatabase? CreateDatabase()
-//        {
-//            return new S3StreamDatabase(new S3InMemoryFactory());
-//        }
-//    }
-//}
+namespace StreamStore.S3.Tests.InMemory
+{
+    internal class S3InMemorySuite : ITestSuite
+    {
+        public IStreamDatabase? CreateDatabase()
+        {
+            return new S3StreamDatabase(new S3InMemoryFactory());
+        }
+    }
+}
