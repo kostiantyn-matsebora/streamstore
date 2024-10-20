0cd80428fa8691dd55243d2f16e5ff220b23533d
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-20 15:09:01 +0300

    Add more unit tests

---
+++ src/StreamStore.Sql.Tests/Sqlite/SqliteDatabaseConfiguratorTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using StreamStore.SQL.Sqlite;
+
+namespace StreamStore.Sql.Tests.Sqlite
+{
+    public class SqliteDatabaseConfiguratorTests
+    {
+        readonly MockRepository mockRepository;
+
+        readonly Mock<IServiceCollection> mockServiceCollection;
+
+        public SqliteDatabaseConfiguratorTests()
+        {
+            mockRepository = new MockRepository(MockBehavior.Strict);
+
+            mockServiceCollection = mockRepository.Create<IServiceCollection>();
+        }
+
+        SqliteDatabaseConfigurator CreateSqliteDatabaseConfigurator()
+        {
+            return new SqliteDatabaseConfigurator(
+                mockServiceCollection.Object);
+        }
+
+        [Fact]
+        public void Configure_Should_CreateFullConfiguration()
+        {
+            // Arrange and Act
+            var configuration = CreateSqliteDatabaseConfigurator()
+                .WithTable("tableName")
+                .WithConnectionString("connectionString")
+                .WithSchemaProvisioning(true)
+                .WithProfiling()
+                .Build();
+
+            
+
+            // Assert
+            configuration.EnableProfiling.Should().BeTrue();
+            configuration.ProvisionSchema.Should().BeTrue();
+            configuration.ConnectionString.Should().Be("connectionString");
+            configuration.TableName.Should().Be("tableName");
+
+            mockRepository.VerifyAll();
+        }
+    }
+}
