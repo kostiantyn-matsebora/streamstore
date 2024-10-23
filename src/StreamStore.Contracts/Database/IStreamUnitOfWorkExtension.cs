eaf072ff49bb1297821f27e372e6f6b266b97443
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-10-23 10:41:06 +0300

    Add more writing scenarios

---
+++ src/StreamStore.Contracts/Database/IStreamUnitOfWorkExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamStore
+{
+    public static class IStreamUnitOfWorkExtension
+    {
+        public static async Task<IStreamUnitOfWork> AddAsync(this Task<IStreamUnitOfWork> uow, Id eventId, DateTime timestamp, byte[] data, CancellationToken token = default)
+        {
+            await uow.Result.AddAsync(eventId, timestamp, data, CancellationToken.None);
+            return uow.Result;
+        }
+
+        public static async Task<IStreamUnitOfWork> SaveChangesAsync(this Task<IStreamUnitOfWork> unitOfWork, CancellationToken token = default)
+        {
+            await unitOfWork.Result.SaveChangesAsync(token);
+            return unitOfWork.Result;
+        }
+    }
+}
--- src/StreamStore.Testing/Scenarios/StreamDatabase/Writing_to_database.cs
+++ src/StreamStore.Testing/Scenarios/StreamDatabase/Writing_to_database.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using StreamStore.Exceptions;
 
 namespace StreamStore.Testing.Scenarios.StreamDatabase
 {
@@ -8,9 +9,11 @@ namespace StreamStore.Testing.Scenarios.StreamDatabase
         {
         }
 
-        [Fact]
+        [SkippableFact]
         public async Task When_parameters_are_absent_or_incorrect()
         {
+            TrySkip();
+
             // Arrange
             var eventId = Id.None;
             var stream = Container.GetExistingStream();
@@ -34,6 +37,94 @@ namespace StreamStore.Testing.Scenarios.StreamDatabase
 
             //Assert
             await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        [SkippableTheory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public async Task When_expected_revision_greater_than_actual(int increment)
+        {
+            TrySkip();
+
+            // Arrange
+            var eventId = Id.None;
+            var stream = Container.GetExistingStream();
+
+            // Act
+            var act = async () => await Database.BeginAppendAsync(stream.Id, stream.Length + increment);
+
+            // Assert
+            await act.Should().ThrowAsync<OptimisticConcurrencyException>();
+
+            // Arrange
+            var uow = await Database.BeginAppendAsync(stream.Id, stream.Length);
+
+            await Database.BeginAppendAsync(stream.Id, stream.Length)
+                .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                .SaveChangesAsync();
+
+            // Act
+            act = () => uow.AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray).SaveChangesAsync();
+
+            //Assert
+            await act.Should().ThrowAsync<OptimisticConcurrencyException>();
+        }
+
+        [SkippableTheory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task When_writing_many_times(bool isNew)
+        {
+            TrySkip();
+
+            // Arrange
+            Id streamId = GeneratedValues.Id;
+            Revision revision = Revision.Zero;
+            if (!isNew)
+            {
+                var stream = Container.GetExistingStream();
+                streamId = stream.Id;
+                revision = stream.Length;
+            }
+         
+            // Act
+            
+            await Database.BeginAppendAsync(streamId, revision)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .SaveChangesAsync();
+
+            // Assert
+            var metadata = await Database.FindMetadataAsync(streamId);
+            metadata.Should().NotBeNull();
+            metadata!.Revision.Should().Be(revision + 2);
+
+            // Act
+            await Database.BeginAppendAsync(streamId, revision + 2)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                   .SaveChangesAsync();
+
+            // Assert
+            metadata = await Database.FindMetadataAsync(streamId);
+            metadata.Should().NotBeNull();
+            metadata!.Revision.Should().Be(revision + 2 + 1);
+
+            // Act
+            await Database.BeginAppendAsync(streamId, revision + 2 + 1)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .AddAsync(GeneratedValues.Id, GeneratedValues.DateTime, GeneratedValues.ByteArray)
+                  .SaveChangesAsync();
+
+            // Assert
+            metadata = await Database.FindMetadataAsync(streamId);
+            metadata.Should().NotBeNull();
+            metadata!.Revision.Should().Be(revision + 2 + 1 + 5);
         }
     }
 }
