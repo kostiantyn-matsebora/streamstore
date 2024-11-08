18adee11b92031dd659a486ef4543a5497952ed7
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-08 21:06:55 +0200

    Add StreamEvent tests

--- src/StreamStore.Tests/Models/Scenarios/Creating.cs
+++ src/StreamStore.Tests/Models/Scenarios/Creating.cs
@@ -7,7 +7,7 @@ namespace StreamStore.Tests.Models
     public class Creating: Scenario
     {
         [Fact]
-        public void When_created_with_collection_of_records()
+        public void When_record_collection_created_with_collection_of_records()
         {
             // Arrange
             var fixture = new Fixture();
@@ -20,5 +20,33 @@ namespace StreamStore.Tests.Models
             collection.Should().HaveSameCount(records);
             collection.Should().BeEquivalentTo(records);
         }
+
+        [Fact]
+        public void When_creating_stream_event_with_inproper_parameters()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var records = fixture.CreateMany<EventMetadataRecord>(10);
+
+            // Act
+            var act = () => new StreamEvent(Id.None, Generated.Revision, Generated.DateTime, Generated.Object<object>());
+
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>();
+
+            // Act
+            act = () => new StreamEvent(Generated.Id, Generated.Revision, default, Generated.Object<object>());
+
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>();
+
+            // Act
+            act = () => new StreamEvent(Generated.Id, Generated.Revision, Generated.DateTime,  null!);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+
+        }
+
     }
 }
