7ad0997152991d6aea54be79f5d432691aa46a72
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-12 10:02:09 +0200

    Move benchmarking to separate branch

--- src/StreamStore.S3.IntegrationTests/Old/AWS/AWSS3ClientIntegrationTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreamStore.S3.IntegrationTests.Old.AWS
-{
-    public class AWSS3ClientIntegrationTests : S3ClientIntegrationTestsBase<AWSS3TestsSuite>
-    {
-        public AWSS3ClientIntegrationTests() : base(new AWSS3TestsSuite())
-        {
-        }
-    }
-}
