805fb2c196e9a412c5e0b0e037b2bff1bfc61569
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-12 10:05:30 +0200

    Move unfinished benchmarking and S3 integration tests to separate branches

--- src/StreamStore.S3.IntegrationTests/Old/AWS/AWSS3StreamLockIntegrationTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreamStore.S3.IntegrationTests.Old.AWS
-{
-    public class AWSS3StreamLockIntegrationTests : S3StreamLockIntegrationTests<AWSS3TestsSuite>
-    {
-        public AWSS3StreamLockIntegrationTests() : base(new AWSS3TestsSuite())
-        {
-        }
-    }
-}
--- src/StreamStore.S3.IntegrationTests/Old/B2/B2S3StreamLockIntegrationTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StreamStore.S3.IntegrationTests.Old.B2
-{
-    public class B2S3StreamLockIntegrationTests : S3StreamLockIntegrationTests<B2S3TestsSuite>
-    {
-        public B2S3StreamLockIntegrationTests() : base(new B2S3TestsSuite())
-        {
-        }
-    }
-}
