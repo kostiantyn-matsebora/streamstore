bdb8f988cc29678531a3fff77d46918eff446902
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-13 14:02:23 +0200

    Add tenant stream factory tests

---
+++ src/StreamStore.Tests/StreamStore/Multitenancy/TenantStreamStoreFactorySuite.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using StreamStore.Multitenancy;
+using StreamStore.Testing;
+using StreamStore.Testing.Framework;
+
+namespace StreamStore.Tests.StreamStore.Multitenancy
+{
+    public class TenantStreamStoreFactorySuite : TestSuiteBase
+    {
+
+        public Mock<ITenantStreamDatabaseProvider> MockTenantStreamDatabaseProvider => Generated.MockOf<ITenantStreamDatabaseProvider>();
+        public Mock<IEventSerializer> MockEventSerializer => Generated.MockOf<IEventSerializer>();
+        public StreamStoreConfiguration Configuration => new StreamStoreConfiguration();
+    }
+}
