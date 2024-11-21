301abf0903133b2f5887191c4be31317fa40831b
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-21 16:00:36 +0200

    Add mapping tests

---
+++ src/StreamStore.NoSQL.Tests/Cassandra/Mapping/Scenarios/Configuring_mapping.cs
@@ -0,0 +1,42 @@
+﻿using Cassandra.Mapping;
+using FluentAssertions;
+using StreamStore.NoSql.Cassandra.Configuration;
+using StreamStore.NoSql.Cassandra.Database;
+using StreamStore.NoSql.Cassandra.Models;
+using StreamStore.Testing;
+
+namespace StreamStore.NoSql.Tests.Cassandra.Mapping
+{
+    public class Configuring_mapping: Scenario
+    {
+
+        [Fact]
+        public void When_configuring_mapping()
+        {
+            // Arrange
+            var config = new CassandraStorageConfiguration();
+
+            // Act
+            var mapping = new MappingConfiguration().Define(new CassandraStreamMapping(config));
+
+            // Assert
+            mapping.Get<EventEntity>().TableName.Should().Be(config.EventsTableName);
+            mapping.Get<EventMetadataEntity>().TableName.Should().Be(config.EventsTableName);
+            mapping.Get<RevisionStreamEntity>().TableName.Should().Be(config.EventsTableName);
+            mapping.Get<EventEntity>().PartitionKeys.Should().Contain("stream_id");
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventEntity.StreamId))).Should().NotBeNull();
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventEntity.Id))).Should().NotBeNull();
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventEntity.Revision))).Should().NotBeNull();
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventEntity.Data))).Should().NotBeNull();
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventEntity.Timestamp))).Should().NotBeNull();
+            mapping.Get<EventMetadataEntity>().PartitionKeys.Should().Contain("stream_id");
+            mapping.Get<EventMetadataEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventMetadataEntity.StreamId))).Should().NotBeNull();
+            mapping.Get<EventMetadataEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventMetadataEntity.Id))).Should().NotBeNull();
+            mapping.Get<EventMetadataEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventMetadataEntity.Revision))).Should().NotBeNull();
+            mapping.Get<EventEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(EventMetadataEntity.Timestamp))).Should().NotBeNull();
+            mapping.Get<RevisionStreamEntity>().PartitionKeys.Should().Contain("stream_id");
+            mapping.Get<RevisionStreamEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(RevisionStreamEntity.StreamId))).Should().NotBeNull();
+            mapping.Get<RevisionStreamEntity>().GetColumnDefinition(typeof(EventEntity).GetProperty(nameof(RevisionStreamEntity.Revision))).Should().NotBeNull();
+        }
+    }
+}
---
+++ src/StreamStore.NoSQL.Tests/Cassandra/Multitenancy/TenantClusterRegistry/Scenarios/Instantiating.cs
@@ -0,0 +1,9 @@
+﻿using StreamStore.Testing;
+
+namespace StreamStore.NoSql.Tests.Cassandra.Multitenancy.TenantClusterRegistry
+{
+    public class Instantiating: Scenario
+    {
+
+    }
+}
