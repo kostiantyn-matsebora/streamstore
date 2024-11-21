591be924055bcac17a83f2cae1b0aff3c3a9b545
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-21 16:03:10 +0200

    Add TenantClusterRegistry tests

--- src/StreamStore.NoSQL.Tests/Cassandra/Multitenancy/TenantClusterRegistry/Scenarios/Instantiating.cs
+++ src/StreamStore.NoSQL.Tests/Cassandra/Multitenancy/TenantClusterRegistry/Scenarios/Instantiating.cs
@@ -1,9 +1,27 @@
-﻿using StreamStore.Testing;
+﻿using Cassandra;
+using FluentAssertions;
+using StreamStore.NoSql.Cassandra.Multitenancy;
+using StreamStore.Testing;
 
 namespace StreamStore.NoSql.Tests.Cassandra.Multitenancy.TenantClusterRegistry
 {
     public class Instantiating: Scenario
     {
+        [Fact]
+        public void When_any_of_parameters_is_not_set()
+        {
 
+            // Act
+            var act = () => new CassandraTenantClusterRegistry(Cluster.Builder(), null);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+
+            // Act
+            act = () => new CassandraTenantClusterRegistry(null, new DelegateTenantClusterConfigurator());
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>();
+        }
     }
 }
