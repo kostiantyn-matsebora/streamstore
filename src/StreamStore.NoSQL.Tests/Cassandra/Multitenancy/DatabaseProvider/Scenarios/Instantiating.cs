4d4dcdf7d106b683a2dad118528e97243dfdc88a
kostiantyn-matsebora <kmatsebora@gmail.com>
2024-11-25 17:15:23 +0200

    Add Database provider tests

--- src/StreamStore.NoSQL.Tests/Cassandra/Multitenancy/DatabaseProvider/Scenarios/Instantiating.cs
+++ src/StreamStore.NoSQL.Tests/Cassandra/Multitenancy/DatabaseProvider/Scenarios/Instantiating.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
+using StreamStore.NoSql.Cassandra.API;
+using StreamStore.NoSql.Cassandra.Multitenancy;
+using StreamStore.Testing;
 
-namespace StreamStore.NoSql.Tests.Cassandra.Multitenancy.DatabaseProvider.Scenarios
+namespace StreamStore.NoSql.Tests.Cassandra.Multitenancy.DatabaseProvider;
+
+public class Instantiating : Scenario
 {
-    internal class Instantiating
+    public void When_any_argument_is_not_set()
     {
+        // Act
+        var act = () => new CassandraStreamDatabaseProvider(Generated.MockOf<ICassandraTenantMapperProvider>().Object, null!);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+        // Act
+        act = () => new CassandraStreamDatabaseProvider(null!, Generated.MockOf<ICassandraTenantStorageConfigurationProvider>().Object);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
     }
 }
