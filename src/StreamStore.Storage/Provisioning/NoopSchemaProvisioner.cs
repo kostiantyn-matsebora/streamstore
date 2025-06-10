852988f702bcc8c2bdc9b6a89ae905be8a1cba1b
kostiantyn-matsebora <kmatsebora@gmail.com>
2025-06-10 19:20:37 +0300

    Exclude noop schema provisioner from code coverage

--- src/StreamStore.Storage/Provisioning/NoopSchemaProvisioner.cs
+++ src/StreamStore.Storage/Provisioning/NoopSchemaProvisioner.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using System.Threading.Tasks;
 using StreamStore.Provisioning;
 
 namespace StreamStore.Storage.Provisioning
 {
+    [ExcludeFromCodeCoverage]
     internal class NoopSchemaProvisioner : ISchemaProvisioner
     {
         public Task ProvisionSchemaAsync(CancellationToken token)
