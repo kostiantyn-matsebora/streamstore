fdc17fb0a9467393c5107df1cab1a0db32093887
kostiantyn-matsebora <kmatsebora@gmail.com>
2025-06-10 19:07:55 +0300

    Add tests for delegate storage provider

---
+++ src/StreamStore.Tests/Multitenancy/Scenarios/Provide_storage.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using StreamStore.Storage.Multitenancy;
+using StreamStore.Testing;
+
+namespace StreamStore.Tests.Multitenancy
+{
+    public class Provide_storage: Scenario
+    {
+        [Fact]
+        public void When_delegate_is_not_defined()
+        {
+            var act = () => new DelegateStorageProvider(null!);
+            act.Should().Throw<ArgumentNullException>().WithParameterName("provider");
+        }
+
+        [Fact]
+        public void Should_invoke_delegate_when_creating_provisioner()
+        {
+            // Arrange
+            var tenantId = Generated.Primitives.Id;
+            var storage = Generated.Mocks.Single<IStreamStorage>();
+            var provider = new DelegateStorageProvider(_ => storage.Object);
+
+            // Act
+            var createdStorage = provider.GetStorage(tenantId);
+
+            // Assert
+            createdStorage.Should().Be(storage.Object);
+        }
+    }
+}
